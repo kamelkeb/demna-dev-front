eeaeca90206d42c0da73a6ab07a3290445c06296
s4m75 <contact8395@laposte.net>
2019-09-30 19:41:37 +0200

    ajout routes

---
+++ src/routes/client_routes.js
@@ -0,0 +1,33 @@
+export const pathHome = "/";
+
+export const pathConnection = pathHome + "connection";
+export const pathInscription = pathHome + "inscription";
+export const pathOubli = pathHome + "oubli";
+export const pathConsultationAppel = pathHome + "consultation_appel";
+export const pathVolontariatAppel = pathHome + "volontariat_appel";
+export const pathApropos = pathHome + "apropos";
+export const pathContact = pathHome + "contact";
+
+const pathDepotAppel = pathHome + "depot_appel";
+const pathGestionNotifications = pathHome + "gestion_notifications";
+const pathAjoutModifNotification = pathHome + "ajout_modif_notification";
+const pathFicheUtilisateur = pathHome + "fiche_utilisateur";
+const pathGestionSignalements = pathHome + "gestion_signalements";
+const pathFicheSignalement = pathHome + "fiche_signalement";
+const pathSignalement = pathHome + "signalement";
+
+const idPath = pathHome + "{id}";
+
+export const idPathConsultationAppel = idPath + pathConsultationAppel;
+export const idPathVolontariatAppel = idPath + pathVolontariatAppel;
+export const idPathApropos = idPath + pathApropos;
+export const idPathContact = idPath + pathContact;
+
+export const idPathAnnulationParticipation = idPath + pathAnnulationParticipation;
+export const idPathDepotAppel = idPath + pathDepotAppel;
+export const idPathGestionNotifications = idPath + pathGestionNotifications;
+export const idPathAjoutModifNotification = idPath + pathAjoutModifNotification;
+export const idPathFicheUtilisateur = idPath + pathFicheUtilisateur;
+export const idPathGestionSignalements = idPath + pathGestionSignalements;
+export const idPathFicheSignalement = idPath + pathFicheSignalement;
+export const idPathSignalement = idPath + pathSignalement;
---
+++ src/routes/server_routes.js
@@ -0,0 +1,10 @@
+const apiPathHome = "/api";
+export const statPathApropos = "/static/apropos";
+
+export const apiPathAppels = apiPathHome + "/appels";
+export const apiPathContact = apiPathHome + "/contact";
+
+export const apiPathAuthentification = apiPathHome + "/authentification";
+export const apiPathUtilisateurs = apiPathHome + "/utilisateurs";
+export const apiPathSignalements = apiPathHome + "/signalements";
+export const apiPathNotifications = apiPathHome + "/notifications";
