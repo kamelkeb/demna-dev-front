e86a63ae602fe3e086eae5373dae14998b51dab7
Ajr Wum <ajr.wum@gmail.com>
2019-09-30 15:20:02 +0200

    Ajout des dependances Semantic-UI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+// import pour Semantic-UI
+import 'semantic-ui-css/semantic.min.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
